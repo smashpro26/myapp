e10991141a4163f2a34340bf332d62bc7863b8d3
suvir <mrsuvir@gmail.com>
2023-07-28 19:00:17 +0100

    Revert "testCommit"
    This reverts commit f7c216c40442df8b842daba705fa6a718b762e04.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -33,7 +33,7 @@ class HomePage extends StatelessWidget {
                 suffixIcon: Padding(
                   padding: const EdgeInsets.all(12),
                   child: SvgPicture.asset(
-                    'assets/search.svg',
+                    'assets/filter.svg',
                     width: 20,
                     height: 20,
                   ),
